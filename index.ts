d78cc1a6ee348c64a3ad9f47bd336502c8308adc
Sidoine De Wispelaere <sidoine@sidoine.net>
2017-10-17 21:06:26 +0200

    It is not an embedable library

--- index.ts
+++ index.ts
@@ -1,16 +1,10 @@
 import { Constructor, Library} from "@wowts/tslib";
 
-export interface AceConfig {
-    RegisterOptionsTable(appName: string, options: any, title?:string):void;
+export class AceConfig {
+    RegisterOptionsTable(appName: string, options: any, title?: string):void {
+    }
 }
 
-const lib: Library<AceConfig> = {
-    Embed<T extends Constructor<{}>>(Base: T): Constructor<AceConfig> & T {
-        return class extends Base {
-            public RegisterOptionsTable(appName: string, options: any, title?:string):void {
-            }
-        };
-    },
-};
+const lib = new AceConfig();
 export default lib;
 
