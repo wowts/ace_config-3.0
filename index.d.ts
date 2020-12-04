888a5252e8bd0992abf6243f6ce53e4a583a926b
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 12:56:06 +0100

    1.2.0

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class AceConfig {
-    RegisterOptionsTable(appName: string, options: any, title?: string): void;
-}
-declare const lib: AceConfig;
-export default lib;
--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AceConfig {
-    RegisterOptionsTable(appName, options, title) {
-    }
-}
-exports.AceConfig = AceConfig;
-const lib = new AceConfig();
-exports.default = lib;
